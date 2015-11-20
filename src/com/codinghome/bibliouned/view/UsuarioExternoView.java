71c22283913e8200472a4224597bac4e4d8d139b
Daniel Sánchez <da.sanchez.suarez@gmail.com>
2015-11-20 20:43:46 +0100

    Missing classes

---
+++ src/com/codinghome/bibliouned/view/UsuarioExternoView.java
@@ -0,0 +1,92 @@
+package com.codinghome.bibliouned.view;
+
+public class UsuarioExternoView {
+	
+	private String identificador;
+	private String biblioteca;
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	private String nifPasaporte;
+	private String mail;
+	private String telefono;
+	private String localidad;
+	private String codigoPostal;
+	private String direccion;
+	private String observaciones;
+	
+	public UsuarioExternoView() {}
+	
+	public String getIdentificador() {
+		return identificador;
+	}
+	public void setIdentificador(String identificador) {
+		this.identificador = identificador;
+	}
+	public String getBiblioteca() {
+		return biblioteca;
+	}
+	public void setBiblioteca(String biblioteca) {
+		this.biblioteca = biblioteca;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido1() {
+		return apellido1;
+	}
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+	public String getApellido2() {
+		return apellido2;
+	}
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+	public String getNifPasaporte() {
+		return nifPasaporte;
+	}
+	public void setNifPasaporte(String nifPasaporte) {
+		this.nifPasaporte = nifPasaporte;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	public String getCodigoPostal() {
+		return codigoPostal;
+	}
+	public void setCodigoPostal(String codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getObservaciones() {
+		return observaciones;
+	}
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
+}
